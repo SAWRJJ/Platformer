04cb26444955bcc99729779d4fd8ade983c48eef
SAWRJJ <jjr1139802674@gmail.com>
2025-02-15 15:21:12 +0800

    Update main.lua

---
+++ main.lua
@@ -0,0 +1,17 @@
+function love.load()
+    wf = require 'libraries/windfield/windfield'
+    -- 改变重力方向
+    world = wf.newWorld(0, 800) 
+
+    player = world:newRectangleCollider(360, 100, 80, 80)
+    platform = world:newRectangleCollider(250, 499, 300, 100)
+    platform:setType('static')
+end
+
+function love.update(dt)
+    world:update(dt)
+end
+
+function love.draw()
+    world:draw()
+end
