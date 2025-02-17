eb2723bcaa50dea2a86ce94d9cedb088a07dbf2b
SAWRJJ <jjr1139802674@gmail.com>
2025-02-17 19:45:35 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,14 +4,30 @@ function love.load()
     world = wf.newWorld(0, 800) 
 
     player = world:newRectangleCollider(360, 100, 80, 80)
+    player.speed = 240
     platform = world:newRectangleCollider(250, 499, 300, 100)
     platform:setType('static')
 end
 
 function love.update(dt)
     world:update(dt)
+
+
+    local px, py = player:getPosition()
+    if love.keyboard.isDown('d') then
+        player:setX(px + player.speed*dt)
+    end
+    if love.keyboard.isDown('a') then
+        player:setX(px - player.speed*dt)
+    end
 end
 
 function love.draw()
     world:draw()
+end
+
+function love.keypressed(key)
+    if key == 'w' then
+        player:applyLinearImpulse(0, -5000)
+    end
 end
