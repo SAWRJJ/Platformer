6acd2a5bda249810bfb5b1ecb6fd10067ef0a452
SAWRJJ <jjr1139802674@gmail.com>
2025-02-18 20:49:30 +0800

    anim8

--- main.lua
+++ main.lua
@@ -1,4 +1,15 @@
 function love.load()
+    anim8 = require "libraries/anim8/anim8"
+    sprites = {}
+    sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+
+    local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    
+    animations = {}
+    animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
+    animations.junp = anim8.newAnimation(grid('1-7', 2), 0.05)
+    animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+
     wf = require 'libraries/windfield/windfield'
     -- 改变重力方向
     world = wf.newWorld(0, 800, false) 
@@ -10,6 +21,7 @@ function love.load()
     player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "player"})
     player:setFixedRotation(true)
     player.speed = 240
+    player.animation = animations.run
 
     platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "platform"})
     platform:setType('static')
@@ -35,10 +47,14 @@ function love.update(dt)
             player:destroy()
         end
     end
+
+    player.animation:update(dt)
+
 end
 
 function love.draw()
     world:draw()
+    player.animation:draw(sprites.playerSheet, 0, 0)
 end
 
 function love.keypressed(key)
