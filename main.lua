55ddbc15fc64cdf54f343a69039c25edcbc9aef6
SAWRJJ <jjr1139802674@gmail.com>
2025-02-18 16:13:43 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ function love.load()
     wf = require 'libraries/windfield/windfield'
     -- 改变重力方向
     world = wf.newWorld(0, 800, false) 
+    world:setQueryDebugDrawing(true)
 
     world:addCollisionClass('platform')
     world:addCollisionClass('player' --[[, {ignores = {'platform'}}]])
@@ -41,7 +42,19 @@ function love.draw()
 end
 
 function love.keypressed(key)
-    if key == 'w' then
-        player:applyLinearImpulse(0, -5000)
+    if key == 'space' then
+        local colliders = world:queryRectangleArea(player:getX() - 40, player:getY()+40, 80, 2, {'platform'})
+        if #colliders > 0 then
+            player:applyLinearImpulse(0, -6000)
+        end
+    end
+end
+
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        local colliders = world:queryCircleArea(x, y, 200, {'platform', 'danger'})
+        for i,c in ipairs(colliders) do
+            c:destory()
+        end
     end
 end
