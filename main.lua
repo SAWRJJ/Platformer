3f7f557862aa9f4452b86400f0992e6b2c799fb8
SAWRJJ <jjr1139802674@gmail.com>
2025-02-27 19:50:18 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -26,7 +26,7 @@ function love.load()
     world:setQueryDebugDrawing(true)
 
     world:addCollisionClass('Platforms')
-    world:addCollisionClass('player' --[[, {ignores = {'platform'}}]])
+    world:addCollisionClass('Player' --[[, {ignores = {'platform'}}]])
     world:addCollisionClass('Danger')
 
     require('player')
@@ -37,7 +37,12 @@ function love.load()
 
     platforms = {}
 
-    loadMap()
+    flagX = 0
+    flagY = 0
+
+    currentLeval = "level1"
+
+    loadMap(currentLeval)
 
     
 end
@@ -51,6 +56,15 @@ function love.update(dt)
     local px, py = player:getPosition()
 
     cam:lookAt(px, love.graphics.getHeight()/2)
+
+    local colliders = world:queryCircleArea(flagX, flagY, 10, {"Player"})
+    if #colliders > 0 then
+        if currentLeval == "level1" then
+            loadMap("level2")
+        elseif currentLeval == "level2" then
+            loadMap("level1")
+        end
+    end
 end
 
 function love.draw()
@@ -69,13 +83,16 @@ function love.keypressed(key)
             
         end
     end
+    -- if key == 'r' then
+    --     loadMap("level2")
+    -- end
 end
 
 function love.mousepressed(x, y, button)
     if button == 1 then
         local colliders = world:queryCircleArea(x, y, 200, {'Platforms', 'Danger'})
         for i,c in ipairs(colliders) do
-            c:destory()
+            c:destroy()
         end
     end
 end
@@ -88,13 +105,41 @@ function spawnPlatform(x, y, width, height)
     end
 end
 
-function loadMap()
-    gameMap = sti("maps/level1.lua")
+function destoryAll()
+    local i = #platforms
+    while i>-1 do
+        if platforms[i] ~= nil then
+            platforms[i]:destroy()
+        end
+        table.remove(platforms, i)
+        i = i-1
+    end
+
+    local i = #enemies
+    while i>-1 do
+        if enemies[i] ~= nil then
+            enemies[i]:destroy()
+        end
+        table.remove(enemies, i)
+        i = i-1
+    end
+end
+
+function loadMap(mapName)
+    currentLeval = mapName
+    destoryAll()
+    player:setPosition(300,100)
+    gameMap = sti("maps/" .. mapName .. ".lua")
     for i, obj in pairs(gameMap.layers["Platforms"].objects) do
         spawnPlatform(obj.x, obj.y ,obj.width, obj.height)
     end
 
     for i, obj in pairs(gameMap.layers["Enemies"].objects) do
         spawnEnemy(obj.x, obj.y)
+    end
+
+    for i, obj in pairs(gameMap.layers["Flag"].objects) do
+        flagX = obj.x
+        flagY = obj.y
     end
 end
